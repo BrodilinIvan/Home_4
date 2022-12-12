8aa783b70709dc374caafd3e984f1050895de097
brodilin ivan <brodilinivan@yandex.ru>
2022-12-12 15:25:28 +0400

    многое не понятно

--- Program.cs
+++ Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+	
+// Console.Write("Введите число: ");
+// int A = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите степень: ");
+// int B = int.Parse(Console.ReadLine());
+
+// Console.WriteLine($"{A} в степени {B} = {Pow(A, B)}");
+
+// int Pow(int num, int rank)
+// {
+//     if (rank ==0) return 1;
+//     else if (rank == 1) return num;
+//     else if (num == 0 || num == 1) return num;
+
+//     int result = num;
+//     for (int i =2; i <= rank; i++)
+//     {
+//         result *= num;
+//     }
+//     return result;
+// }
+
+
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+// Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
+
+// int GetSumNums(int number)
+// {
+//     int sum = 0;
+//     while (number > 0)
+//     {
+//         sum += number % 10;
+//         number /= 10;
+//     }
+//     return sum;
+// }
+
+
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+// int[] array = GetRandomArray(8, 0, 100);
+// Console.WriteLine($"[{String.Join(",", array)}]");
+
+// int[] GetRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] result = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         result[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return result;
+// }
+
+
+
